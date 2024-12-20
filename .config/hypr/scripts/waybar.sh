ffbbf8597a7c56b24ae746f1c45bed7ea544a2de
gitduk <wukaigee@gmail.com>
2024-12-20 18:17:21 +0800

    feat: add new Zed settings and database file - Added `.config/zed/settings.json` for Zed editor configuration. - Introduced `.my.db` for database purposes.
    chore: update Hyprland, Navi, and Waybar configurations
    - Modified `.config/hypr/scripts/waybar.sh` to enhance Waybar script functionality.
    - Updated `.config/navi/cheats/hyprland.cheat` with new cheat entries.
    - Adjusted `.config/waybar/modules` for better module setup.
    
    refactor: clean up unused configurations
    - Removed obsolete `.etc/frps.toml` configuration file.
    - Deleted unused `.etc/vimium_c.json`.
    
    style: update `.zshrc` for environment improvement
    - Refined `.zshrc` settings for enhanced user experience.

--- .config/hypr/scripts/waybar.sh
+++ .config/hypr/scripts/waybar.sh
@@ -18,55 +18,28 @@ find_command() {
   find "$1" -maxdepth 1 -type f -exec basename {} \; | sort
 }
 
-ln_command() {
-  local source_file="$1"
-  local link_file="$2"
-  [[ ! -f "$source_file" ]] && exit 0
-  echo "$source_file"
-  ln -sf "$source_file" "$link_file"
-}
-
-layer_set() {
-  local layer_dir=$1
-  local preview="cat $layer_dir/{}"
-  local window="right:60%:wrap"
-
-  # select waybar layer use fzf
-  selected="$(find_command $layer_dir | fzf --preview=$preview --preview-window=$window)"
-  ln_command "$layer_dir/$selected" "$WAYBAR_CONFIG"
-
-  # reload waybar config
-  if pidof waybar &>/dev/null; then
-    killall -SIGUSR2 waybar
-  else
-    nohup waybar &>/dev/null &
-    sleep 0.5
-  fi
-}
-
 style_set() {
   local style_dir=$1
   local preview="cat $style_dir/{}"
   local window="right:60%:wrap"
+  local link="$2"
 
   # select waybar style use fzf
-  selected="$(find_command $style_dir | fzf --preview=$preview --preview-window=$window)"
-  ln_command "$style_dir/$selected" "$WAYBAR_STYLE"
-
-  # restart waybar
-  pidof waybar &>/dev/null && pkill waybar
-  nohup waybar &>/dev/null &
-  sleep 0.5
+  find_command $style_dir | fzf \
+    --preview=$preview \
+    --preview-window=$window \
+    --bind "Y:execute(ln -sf $style_dir/{} $link && pidof waybar &>/dev/null && pkill waybar && nohup waybar &>/dev/null &)" \
+    --bind "enter:execute(ln -sf $style_dir/{} $link && pidof waybar &>/dev/null && pkill waybar && nohup waybar &>/dev/null &)+abort"
 }
 
 while true; do
   case "$1" in
     -l|--layer)
-      layer_set "$2"
+      style_set "$2" "$WAYBAR_CONFIG"
       shift 2
       ;;
     -s|--style)
-      style_set "$2"
+      style_set "$2" "$WAYBAR_STYLE"
       shift 2
       ;;
     --) shift ; break ;;
